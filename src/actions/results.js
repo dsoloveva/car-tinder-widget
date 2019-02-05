f7db1f4061d9f4774f1094f54ffc8954a2a6b38c
Anna <anna@MacBook-Pro-Dmitry.local>
2019-02-05 17:45:01 +0300

    inventory api

--- src/actions/results.js
+++ src/actions/results.js
@@ -1,15 +1,22 @@
 import { SET_RESULT } from '../constants/actions';
 import { setLoadingStatus, setFinishedStatus, startFetching } from './status';
 import { getRecommendations } from '../requests/tinder';
+import { findInventories } from '../requests/inventory';
 
 export const setResultData = (data) => ({ type: SET_RESULT, data });
 
-export const loadResult = () => (dispatch, getState) => {
+export const loadResult = () => async (dispatch, getState) => {
     const state = getState();
     dispatch(startFetching());
-    return getRecommendations(state.selectedTags)
-      .then((result) => {
-        dispatch(setResultData(result));
-        dispatch(setFinishedStatus());
-      })
+    const recommendations = await getRecommendations(state.selectedTags);
+    const result = await Promise.all(recommendations.map(async (makeModel) => {
+      const inventories = await findInventories(makeModel.make, makeModel.model);
+      return {
+        make: makeModel.make,
+        model: makeModel.model,
+        inventories,
+      };
+    }));
+    dispatch(setResultData(result));
+    dispatch(setFinishedStatus());
   };
---
+++ src/requests/inventory.js
@@ -0,0 +1,19 @@
+import 'isomorphic-fetch';
+
+const zipCode = 44241;
+const radius = 500;
+const pagesize = 3;
+
+export function findInventories(make, model) {
+    const url = `https://www.edmunds.com/api/inventory/v5/find?zip=${zipCode}&sortby=_geo&radius=${radius}&pagenum=1&pagesize=${pagesize}&vehicleInfo.styleInfo.make=${make}&vehicleInfo.styleInfo.model=${model}&fields=vin,vehicleInfo`;
+    const requestConfig = {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'omit',
+    };
+    return fetch(url, requestConfig).then(response => {
+        const dataPromise = response.json();
+        if (response.status === 200) return dataPromise;
+        return Promise.reject('Server error');
+    });
+  }
