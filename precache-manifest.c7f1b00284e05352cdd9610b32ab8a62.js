cc9766e762204f9a549faf41f1873aee517d3152
Daria Soloveva <Daria_Soloveva@epam.com>
2019-02-05 15:30:42 +0300

    Updates

---
+++ precache-manifest.c7f1b00284e05352cdd9610b32ab8a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bffcf530caa54503da0",
+    "url": "/car-tinder/static/js/runtime~main.6bffcf53.js"
+  },
+  {
+    "revision": "4dcb08bb8188c0239e51",
+    "url": "/car-tinder/static/js/main.4dcb08bb.chunk.js"
+  },
+  {
+    "revision": "911790f111af419da674",
+    "url": "/car-tinder/static/js/1.911790f1.chunk.js"
+  },
+  {
+    "revision": "4dcb08bb8188c0239e51",
+    "url": "/car-tinder/static/css/main.d5d8b07c.chunk.css"
+  },
+  {
+    "revision": "e3095e5ed2fbc5d4c1558f8a7c0ff5da",
+    "url": "/car-tinder/index.html"
+  }
+];
